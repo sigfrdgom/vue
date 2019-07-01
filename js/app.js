7fa1c97cfe777ed998e9253ad3f9fa288351d12a
sigfrid <sigfridgom@gmail.com>
2019-06-30 21:10:58 -0600

    primer commit

---
+++ js/app.js
@@ -0,0 +1,60 @@
+var contenido = document.querySelector('#cuerpo-tabla');
+
+document.querySelector('#boton').addEventListener('click',ajax);
+// document.querySelector('#boton').addEventListener('click',traer);
+
+// Trae los datos del REST a traves de FETCH
+function traer() {
+    fetch('http://localhost/codeigniter/api/usuarios')
+    .then(res => res.json())
+    .then(datos => {
+        fillTable(datos);
+    })
+}
+
+function fillTable(datos) {
+
+    contenido.innerHTML = '';
+
+    for (let dato of datos) {
+        contenido.innerHTML += 
+            `
+            <tr>
+                <td><b>${dato.id}</b></td>
+                <td>${dato.nombre}</td>
+                <td>${dato.apellido}</td>
+                <td>
+                    <button class="btn btn-danger" onclick="eliminar(${dato.id})">Eliminar</button>
+                    <button class="btn btn-warning" onclick="actualizar(${dato.id})">Editar</button>
+                </td>
+            </tr>
+            `;
+    }
+ console.log('REST cargado')
+}
+
+function eliminar(id) {
+    alert("Esta por eliminar el elemento con id: " +id);
+}
+
+function actualizar(id) {
+    alert("Esta por actualizar el elemento con id: " +id);
+}
+
+// Trae los datos del REST a traves de AJAX
+function ajax() {
+    var http = new XMLHttpRequest();
+    http.open('GET','http://localhost/codeigniter/api/usuarios',true);
+    http.send();
+
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var datos = JSON.parse(this.responseText);
+            fillTable(datos);
+        }
+    }
+
+}
+// http://localhost/codeigniter/api/usuarios
+// http://localhost/codeigniter/
+// http://localhost/vue/
